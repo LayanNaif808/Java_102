62ea4572f23ea4728779638f76385da5f1770619
LayanNaif808 <layannaif808@gmail.com>
2026-07-08 18:13:36 +0300

    Create Ex2.java

---
+++ Ex2.java
@@ -0,0 +1,17 @@
+public class Ex2 {
+
+    // Method that calculates and returns the square of a number (x * x)
+    public static int square(int x) { 
+
+        return x * x; 
+
+    }
+
+    public static void main(String[] args) { 
+
+        // Call the square method for 4 and 3, then print their sum
+        System.out.println(square(4) + square(3));
+
+    }
+}
+
