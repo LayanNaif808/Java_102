5b271171d27fe9504fffba218f04f9879dfe9df6
LayanNaif808 <layannaif808@gmail.com>
2026-07-08 18:24:07 +0300

    Create Ex3.java

---
+++ Ex3.java
@@ -0,0 +1,12 @@
+public class Ex3 {
+
+    public static void main(String[] args) { 
+
+        // Loop from 1 to 3 and print each number with a space
+        for (int i = 1; i <= 3; i++) {
+            System.out.print(i + " "); 
+        }
+
+    }
+}
+
