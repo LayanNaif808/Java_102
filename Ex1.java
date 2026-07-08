a37732ca002bb0eac2b1c103312aa0b7a4c8d9c0
LayanNaif808 <layannaif808@gmail.com>
2026-07-08 18:06:52 +0300

    Create Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,12 @@
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        // Define an array of integers with 4 numbers
+        int[] numbers = {5, 10, 15, 20}; 
+
+        // Access and add the numbers at index 2 (15) and index 3 (20)
+        System.out.println(numbers[2] + numbers[3]);
+
+    }
+}
